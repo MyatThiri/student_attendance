d35f6ac708aedf89f0c7b7181df873c2476288bf
MyatThiri <myatthiriwinhtut@gmail.com>
2018-09-01 09:56:20 +0630

    Subj View Post

--- models/Subj.js
+++ models/Subj.js
@@ -4,9 +4,15 @@ var bcrypt = require('bcrypt-nodejs');
 var Subj = {
   add: function(params,callback){
     var sql = 'INSERT INTO subject (subj_name,code,dept_id) VALUES (?, ?, ?)';
-    params[2] = bcrypt.hashSync(params[2], bcrypt.genSaltSync(8), null);
     return db.query(sql,params,callback);
   },
+
+  findById: function(subj_id,callback){
+    var sql = "SELECT subj_id,subj_name,code,dept_id,DATE_FORMAT(updated, '%d/%m/%Y %H:%i') AS updated FROM subject WHERE subj_id = ?";
+    return db.query (sql,[subj_id],callback);
+  },
+
 }
+
 
 module.exports = Subj;
--- routes/schedule/index.js
+++ routes/schedule/index.js
@@ -18,7 +18,16 @@ router.post ('/addsubj', function(req,res,next){
   var params = [req.body.name,req.body.id,req.body.code,req.body.dept];
   Subj.add(params, function(err,subj){
     if (err) next (err);
-    res.end ('Success');
+    req.flash('warn', 'Insert Success');
+    res.redirect ('/schedule/subjview/'+subj.insertId);
+  });
+});
+
+router.get('/subjview/:id', function(req,res,next){
+  Subj.findById(req.params.id,function(err,subj){
+    if (err) throw err;
+    if(subj.length == 0) next (new Error('Subject data not found!'));
+      res.render('schedule/subj-view',{title:'Subject View', subj:subj[0]});
   });
 });
 module.exports = router;
