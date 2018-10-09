16609c2b36dffb2d1714de875dfe9206e81f93a1
MyatThiri <myatthiriwinhtut@gmail.com>
2018-10-09 12:49:56 +0630

    dbcon

--- dbcon.js
+++ dbcon.js
@@ -2,14 +2,14 @@ var mysql = require('mysql');
 
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'localhost',
-  user            : 'user_SAMS',
-  password        : 'myatthiri',
-  database        : 'db_SAMS'
-  // host            : 'db4free.net',
-  // user            : 'myatthiri',
+  // host            : 'localhost',
+  // user            : 'user_SAMS',
   // password        : 'myatthiri',
-  // database        : 'ptntu_attendance'
+  // database        : 'db_SAMS'
+  host            : 'db4free.net',
+  user            : 'myatthiri',
+  password        : 'myatthiri',
+  database        : 'ptntu_attendance'
 });
 
 module.exports = pool;
